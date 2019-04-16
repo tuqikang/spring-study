76f956160ec570967b9ab29d450e1b55b99745e7
tqk <1282995880@qq.com>
2019-04-16 11:04:16 +0800

    初步整合完成

--- src/main/java/cn/tqktqk/springdemo/MainTest.java
+++ src/main/java/cn/tqktqk/springdemo/MainTest.java
@@ -1,17 +1,11 @@
 package cn.tqktqk.springdemo;
 
-import cn.tqktqk.springdemo.dao.UsersMapper;
 import cn.tqktqk.springdemo.model.entity.Users;
 import cn.tqktqk.springdemo.service.UserService;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -24,11 +18,13 @@ import java.util.List;
  * @Author: tuqikang
  * @Date: 2019-04-15 15:00
  */
+@ContextConfiguration("classpath*:spring-mybatis.xml")
 public class MainTest {
-    public static void main(String[] args) throws IOException {
-        ApplicationContext ctx = null;
-        ctx = new ClassPathXmlApplicationContext("spring-mybatis.xml");
-        UserService userService = (UserService) ctx.getBean("userService");
+
+    @Autowired
+    private UserService userService;
+
+    public void test(){
         List<Users> list = userService.getAll();
         list.stream().forEach(p->{
             System.out.println(p);
@@ -37,5 +33,13 @@ public class MainTest {
         System.out.println("-----");
         Users users = userService.findUserByUserName("1740611104");
         System.out.println(users);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new MainTest().test();
+//        ApplicationContext ctx = null;
+//        ctx = new ClassPathXmlApplicationContext("spring-mybatis.xml");
+//        UserService userService = (UserService) ctx.getBean("userService");
+
     }
 }
